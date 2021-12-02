adaa487a693830279870bd9cf4c3df95570a41c2
luv4code-lfc <luv4code.lc@gmail.com>
2021-12-02 16:14:36 +0530

    Adding the Discovery Service Application Module. closes #2

---
+++ petclinic-discovery-server/src/main/java/com/luv4code/petclinic/discovery/DiscoveryServerApplication.java
@@ -0,0 +1,17 @@
+package com.luv4code.petclinic.discovery;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import javax.swing.*;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+}
